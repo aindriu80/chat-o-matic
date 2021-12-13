8311b75838b6cdbce28cb64c491866c2ad6d8cd8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-12-13 15:23:18 +0000

    Real-time updates from GraphQL server using Subscriptions

--- client/Chat.jsx
+++ client/Chat.jsx
@@ -3,19 +3,28 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
+  useSubscription,
   gql,
   useMutation,
 } from '@apollo/client'
 import { Container, Row, Col, FormInput, Button } from 'shards-react'
+import { WebSocketLink } from '@apollo/client/link/ws'
+
+const link = new WebSocketLink({
+  uri: 'ws://localhost:4000/',
+  options: {
+    reconnect: true,
+  },
+})
 
 const client = new ApolloClient({
+  link,
   uri: 'http://localhost:4000/',
   cache: new InMemoryCache(),
 })
 
 const GET_MESSAGES = gql`
-  query {
+  subscription {
     messages {
       id
       content
@@ -29,7 +38,7 @@ const POST_MESSAGE = gql`
   }
 `
 const Messages = ({ user }) => {
-  const { data } = useQuery(GET_MESSAGES, { pollInterval: 500 })
+  const { data } = useSubscription(GET_MESSAGES)
   if (!data) {
     return null
   }
